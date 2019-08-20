8d959e8d14d4e4c0d89c60eba4791f6213f8a92d
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-08-20 22:58:59 +0200

    initial

---
+++ deploy.ps1
@@ -0,0 +1,152 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+    Deploys an Azure Resource Manager template
+
+ .PARAMETER subscriptionId
+    The subscription id where the template will be deployed.
+
+ .PARAMETER resourceGroupName
+    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
+
+ .PARAMETER resourceGroupLocation
+    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
+
+ .PARAMETER deploymentName
+    The deployment name.
+
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json.
+
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $subscriptionName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $resourceGroupName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $location,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $managerVmName,
+
+ [Parameter(Mandatory=$False)]
+ [string]
+ $managerVmSize = "Standard_D4s_v3",
+
+ [Parameter(Mandatory=$False)]
+ [string]
+ $adminUser = "VM-Administrator",
+
+ [Parameter(Mandatory=$True)]
+ [securestring]
+ $adminPassword
+
+)
+
+<#
+.SYNOPSIS
+    Registers RPs
+#>
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+# copied from https://stackoverflow.com/a/34383413 with a minimal change
+Function ConvertPSObjectToHashtable
+{
+    param (
+        [Parameter(ValueFromPipeline)]
+        $InputObject,
+
+        [switch]
+        $NoRecurse
+    )
+
+    process
+    {
+        if ($null -eq $InputObject) { return $null }
+
+        if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string])
+        {
+            $collection = @(
+                foreach ($object in $InputObject) { ConvertPSObjectToHashtable $object }
+            )
+            
+            Write-Output -NoEnumerate $collection
+        }
+        elseif ($InputObject -is [psobject])
+        {
+            $hash = @{}
+
+            foreach ($property in $InputObject.PSObject.Properties)
+            {
+                $hash[$property.Name] = ConvertPSObjectToHashtable $property.Value
+            }
+
+            $hash
+        }
+        else
+        {
+            $InputObject
+        }
+    }
+}
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+
+# sign in
+#Write-Host "Logging in...";
+#Login-AzureRmAccount;
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionName'";
+Select-AzureRmSubscription -SubscriptionID (Get-AzureRmSubscription -SubscriptionName $subscriptionName).SubscriptionId;
+
+# Register RPs
+$resourceProviders = @("microsoft.network","microsoft.compute","microsoft.devtestlab","microsoft.resources");
+if($resourceProviders.length) {
+    Write-Host "Registering resource providers"
+    foreach($resourceProvider in $resourceProviders) {
+        #RegisterRP($resourceProvider);
+    }
+}
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. Creating resource group '$resourceGroupName' in location '$location'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile .\template.json -TemplateParameterFile .\parameters.json -location $location -adminPassword $adminPassword -adminUsername $adminUser -virtualMachineName $managerVmName -publicIpAddressName "${managerVmName}-ip" -networkInterfaceName "${managerVmName}-ni" -virtualNetworkName "${resourceGroupName}-vnet" -networkSecurityGroupName "${managerVmName}-nsg" -virtualMachineSize $managerVmSize
