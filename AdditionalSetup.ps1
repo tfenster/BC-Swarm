2749cf1939447ef3ca2d9e81a9c09fd85c56982a
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-08-26 16:54:47 +0200

    check if user exists

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -1,3 +1,8 @@
-Write-Host "Set up SUPER user $username"
-New-NavServerUser -UserName $username -Password $securepassword NAV
-New-NavServerUserPermissionSet -UserName $username -PermissionSetId SUPER NAV
+
+if (!(Get-NAVServerUser -ServerInstance NAV -ErrorAction Ignore | Where-Object { $_.UserName -eq $username })) {
+    Write-Host "Set up SUPER user $username"
+    New-NavServerUser -UserName $username -Password $securepassword NAV
+    New-NavServerUserPermissionSet -UserName $username -PermissionSetId SUPER NAV
+} else {
+    Write-Host "User $username already exists"
+}
