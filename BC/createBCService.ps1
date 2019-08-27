456e26a96265ed1e0bf569b320705d54a8c8ad7d
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-08-27 07:56:34 +0200

    - use swarm configs and secrets - make db connection confable - allow folder download

--- BC/createBCService.ps1
+++ BC/createBCService.ps1
@@ -5,7 +5,31 @@ param(
  
  [Parameter(Mandatory=$True)]
  [string]
- $image
+ $image,
+ 
+ [Parameter(Mandatory=$True)]
+ [string]
+ $databaseServer,
+ 
+ [Parameter(Mandatory=$True)]
+ [string]
+ $databaseUserName,
+ 
+ [Parameter(Mandatory=$True)]
+ [string]
+ $navUserName,
+ 
+ [Parameter(Mandatory=$False)]
+ [string]
+ $addInsUrl,
+
+ [Parameter(Mandatory=$False)]
+ [string]
+ $databasePassword = "Passw0rd*123",
+ 
+ [Parameter(Mandatory=$False)]
+ [string]
+ $navPassword = "Start123$"
 )
 
 $publicDnsName = Get-Content -Path "c:\traefik\externaldns.txt"
@@ -29,17 +53,31 @@ $restRule="PathPrefix:${restPart};ReplacePathRegex: ^${restPart}(.*) /NAV/OData/
 $devRule="PathPrefix:${devPart};ReplacePathRegex: ^${devPart}(.*) /NAV/`$1"
 $dlRule="PathPrefixStrip:${dlPart}"
 
+$folders = "folders=c:\run\my=https://github.com/tfenster/BC-Swarm/archive/master.zip\BC-Swarm-master"
+if (-not [string]::IsNullOrEmpty($addInsUrl)) {
+    $folders += ",C:\Program Files\Microsoft Dynamics NAV\140\Service\Add-ins\Custom=$addInsUrl"
+}
+
 docker service create `
---name $name --health-start-period 300s --network $network --hostname $hostname `
+--name $name --health-start-period 900s --health-timeout 900s --network $network --hostname $hostname `
 -e accept_eula=y -e accept_outdated=y -e usessl=n -e webserverinstance=$name -e publicdnsname=$publicDnsName -e $customNavSettings `
--e "folders=c:\run\my=https://github.com/tfenster/BC-Swarm/archive/master.zip\BC-Swarm-master" `
--e "databaseserver=bc-sql.database.windows.net" -e "databasename=testdb123" -e "databaseinstance=" `
--e "databaseusername=SQL-Administrator" -e "databasepassword=Password*123" `
+-e "$folders" `
+-e "databaseserver=$databaseServer" -e "databasename=$name" -e "databaseinstance=" `
+-e "databaseusername=$databaseUserName" -e "databasepassword=$databasePassword" `
+-e "username=$navUserName" -e "password=$navPassword" `
 --label "traefik.web.frontend.rule=$webclientRule" --label "traefik.web.port=80" `
 --label "traefik.soap.frontend.rule=$soapRule" --label "traefik.soap.port=7047" `
 --label "traefik.rest.frontend.rule=$restRule" --label "traefik.rest.port=7048" `
 --label "traefik.dev.frontend.rule=$devRule" --label "traefik.dev.port=7049" `
 --label "traefik.dl.frontend.rule=$dlRule" --label "traefik.dl.port=8080" `
 --label "traefik.enable=true" --label "traefik.frontend.entryPoints=https" `
+--secret "src=bc_swarm_applicationId,target=c:\ConfigsAndSecrets\bc_swarm_applicationId" `
+--secret "src=bc_swarm_accountSecret,target=c:\ConfigsAndSecrets\bc_swarm_accountSecret" `
+--secret "src=bc_swarm_accountSecretkey,target=c:\ConfigsAndSecrets\bc_swarm_accountSecretkey" `
+--config "src=bc_swarm_tenantId,target=c:\ConfigsAndSecrets\bc_swarm_tenantId" `
+--config "src=bc_swarm_resourceGroup,target=c:\ConfigsAndSecrets\bc_swarm_resourceGroup" `
+--config "src=bc_swarm_serverName,target=c:\ConfigsAndSecrets\bc_swarm_serverName" `
+--config "src=bc_swarm_poolName,target=c:\ConfigsAndSecrets\bc_swarm_poolName" `
+--config "src=bc_swarm_originalDatabaseName,target=c:\ConfigsAndSecrets\bc_swarm_originalDatabaseName" `
 --constraint "node.role!=manager" `
 $image
