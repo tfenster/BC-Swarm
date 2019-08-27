0bc335e926ab97289bda60358286e8cadfcd113a
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-08-27 15:51:34 +0200

    create users with default locale de-DE

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -1,7 +1,7 @@
 
 if (!(Get-NAVServerUser -ServerInstance NAV -ErrorAction Ignore | Where-Object { $_.UserName -eq $username })) {
     Write-Host "Set up SUPER user $username"
-    New-NavServerUser -UserName $username -Password $securepassword NAV
+    New-NavServerUser -UserName $username -Password $securepassword NAV -LanguageId 'de-DE' -Force
     New-NavServerUserPermissionSet -UserName $username -PermissionSetId SUPER NAV
 } else {
     Write-Host "User $username already exists"
