80cf6510dce5d2e14facbb4ec247c550946a2ce6
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-09-13 15:58:26 +0200

    run data upgrade if needed

--- AdditionalSetup.ps1
+++ AdditionalSetup.ps1
@@ -6,3 +6,17 @@ if (!(Get-NAVServerUser -ServerInstance NAV -ErrorAction Ignore | Where-Object {
 } else {
     Write-Host "User $username already exists"
 }
+
+Write-Host "Check tenant state and optionally start data upgrade"
+$tenantState = Get-NAVTenant -ServerInstance NAV -tenant default
+
+if ($tenantState.State -eq "OperationalDataUpgradePending") {
+    Write-Host "Tenant default needs data upgrade"
+    Restart-NAVServerInstance NAV
+    Start-NAVDataUpgrade -Tenant default NAV -Force
+    while ($tenantState.State -ne "Operational") {
+        Write-Host "Waiting for upgrade to finish"
+        Start-Sleep -Seconds 30
+        $tenantState = Get-NAVTenant -ServerInstance NAV -tenant default
+    }
+}
