e1456dc2f4a49aa60bcfe5c29d88863e2eef0de9
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-09-17 06:55:10 +0200

    disable event log dumping

---
+++ MainLoop.ps1
@@ -0,0 +1,3 @@
+Write-Host "Run this to get logs: Get-EventLog -LogName Application -Newest 20 -ErrorAction Ignore | Where-Object { ($_.Source -like '*Dynamics*' -or $_.Source -eq $SqlServiceName) -and ($_.EntryType -eq `"Error`" -or $_.EntryType -eq `"Warning`") -and $_.EntryType -ne `"0`" } | Select-Object TimeGenerated, EntryType, Message | format-list"
+
+while ($true) { start-sleep -seconds 10 }
