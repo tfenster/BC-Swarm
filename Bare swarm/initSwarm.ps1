97036f1f14cb1d95ce1df96730b0281f96f9ca33
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-11-14 19:50:37 +0100

    really bare

--- Bare swarm/initSwarm.ps1
+++ Bare swarm/initSwarm.ps1
@@ -12,8 +12,6 @@ param(
 # Swarm setup
 New-NetFirewallRule -DisplayName "Allow Swarm TCP" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 2377,7946 | Out-Null
 New-NetFirewallRule -DisplayName "Allow Swarm UDP" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 4789,7946 | Out-Null
-New-NetFirewallRule -DisplayName "Allow Voting app" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 5000,5001 | Out-Null
-New-NetFirewallRule -DisplayName "Allow Portainer" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 9000 | Out-Null
 
 $ipaddress = (Get-NetIPAddress | Where-Object { $_.IPAddress.StartsWith("10.0.3") }).IPAddress
 $result = Invoke-Expression "docker swarm init --advertise-addr $ipaddress"
@@ -22,9 +20,9 @@ if ([string]::Concat($result) -match "docker swarm join --token (?<Token>.+) ${i
     Write-Host ("docker swarm join --token " + $matches.Token + " ${ipaddress}:2377")
     Invoke-Expression "docker network create --driver=overlay public" | Out-Null
     Start-Sleep -Seconds 10
-    mkdir c:\compose
-    Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/tfenster/BC-Swarm/master/Bare%20swarm/docker-compose.yml" -OutFile c:\compose\docker-compose.yml
-    Invoke-Expression "docker stack deploy -c c:\compose\docker-compose.yml base"
+    #mkdir c:\compose
+    #Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/tfenster/BC-Swarm/master/Bare%20swarm/docker-compose.yml" -OutFile #c:\compose\docker-compose.yml
+    #Invoke-Expression "docker stack deploy -c c:\compose\docker-compose.yml base"
 } else {
     Write-Host "Swarm init has failed: $result"
 }
