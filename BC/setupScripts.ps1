6a6777bc0f3f969387f9ede4c265664f881cf3d4
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-11-16 16:52:45 +0100

    do script setup on init

--- BC/setupScripts.ps1
+++ BC/setupScripts.ps1
@@ -4,10 +4,8 @@ $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
 Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
 Stop-Process -Name Explorer
-Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
 
 mkdir c:\scripts | out-null
 Set-Location C:\scripts
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/tfenster/BC-Swarm/master/BC/createConfigsAndSecrets.ps1" -OutFile C:\scripts\createConfigsAndSecrets.ps1
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/tfenster/BC-Swarm/master/BC/createBCService.ps1" -OutFile C:\scripts\createBCService.ps1
-Write-Host "Script folder prepared and downloaded all scripts" -ForegroundColor Green
